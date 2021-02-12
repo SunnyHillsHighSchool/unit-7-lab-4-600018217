5094166979607a5bac786043b3ed9a38ace28260
Johnathan Mitri <600018217@fjuhsd.org>
2021-02-12 00:43:30 +0000

    wrote lab 4 code

--- Number.java
+++ Number.java
@@ -1,6 +1,6 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Johnathan Mitri
 
 
 //write the Number class
@@ -9,20 +9,58 @@
 //to guide you
 
 //header for Number class
+public class Number
+{
 
 //private instance variable (must be an Integer)
-
+private int value;
 //constructor
+public Number()
+{
+  value = 0;
+}
 
+public Number(int a)
+{
+  value = a;
+}
 
 //getter method
-
+public int getValue()
+{
+  return value;
+}
 
 //setter method
+public void setValue(int newValue)
+{
+  value = newValue;
+}
 
 //isOdd method
+public boolean isOdd()
+{
+  return (value%2) == 1;
+}
 
 //isPerfect method
-
+public boolean isPerfect()
+{
+  int factorSum = 1;
+  for (int i = 2; i < value; i++)
+  {
+    if (value % i == 0)
+    {
+      factorSum += i;
+    }
+  }
+  return value == factorSum;
+}
 
 //toString
+public String toString()
+{
+  return "" + value;
+}
+
+}
